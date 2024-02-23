e3345dad244f2c6886f53a88045ccfabb08638fe
Kaj Lund <kaj.lund@gmail.com>
2024-02-23 18:00:57 +0200

    Initial commit. Cleaned up Vite defaults

---
+++ src/app.js
@@ -0,0 +1,4 @@
+import "./style.css";
+
+// Set copyright year to current year
+document.querySelector("#thisyear").innerText = new Date().getFullYear()
---
+++ vite.config.js
@@ -0,0 +1,3 @@
+import { defineConfig } from "vite"; // VSCode autocomplete
+
+export default defineConfig({});
