b4fb9e9f540fcf461c735a0090620ca493f98dc7
Kaj Lund <kaj.lund@gmail.com>
2024-02-27 18:05:21 +0200

    Adds menu section

---
+++ README.md
@@ -0,0 +1,22 @@
+# DaClient
+
+Website vanilla.js client
+
+## ToDos
+
+- [x] Setup client development environment
+	- [x] Create Vite project setup `npm create vite@latest`
+	- [x] Add tools ESLint `npm init @eslint/config` & Prettier  `npm i -D --save-exact prettier`
+	- [x] Add npm scripts scripts for linting and formatting
+	- [x] Add vite.config.js and export empty config for now
+	- [x] Run linter & formatter
+	- [x] Replace Vite generated stuff with Hello World page with working css and js
+	- [x] Create git repo local and remote, add * and push initial commit
+	- [x] Add README
+- [ ] Add Menu section
+  - [x] Add brand and main menu
+  - [x] Add popup menu for small screens
+  - [x] Add functionality for dark/light modes
+- [ ] Add dummy page navigation
+  - [ ]
+
--- src/app.js
+++ src/app.js
@@ -1,4 +1,53 @@
 import "./style.css";
+import "./assets/reusable.css"
+import "./assets/header.css"
+import "./assets/media.css"
+
+// Generic selector function to throw error if bad query
+const selectElement = (selector) => {
+  const element = document.querySelector(selector);
+  if(element) return element;
+  throw new Error(`Something went wrong! Make sure that ${selector} exists/is typed correctly.`);
+};
+
+// Nav styles on scroll
+window.addEventListener('scroll', () => {
+  const navbarElement = selectElement('#header');
+  if(this.scrollY >= 15) {
+    navbarElement.classList.add('activated');
+  } else {
+    navbarElement.classList.remove('activated');
+  }
+});
+
 
 // Set copyright year to current year
-document.querySelector("#thisyear").innerText = new Date().getFullYear()
+selectElement("#thisyear").innerText = new Date().getFullYear()
+
+// Menu toggle
+const menuToggleIcon = selectElement('#menu-toggle-icon');
+menuToggleIcon.addEventListener('click', () => {
+  const mobileMenu = selectElement('#menu');
+  mobileMenu.classList.toggle('activated');
+  menuToggleIcon.classList.toggle('activated');
+});
+
+// Check if theme was persisted
+const currentTheme = localStorage.getItem('currentTheme');
+if (currentTheme) {
+  document.body.classList.add('light-theme');
+}
+
+// Toggle theme button handler
+const themeToggleBtn = selectElement('#theme-toggle-btn');
+themeToggleBtn.addEventListener('click', () => {
+  document.body.classList.toggle('light-theme');
+
+  // If the body has the class of light theme then add it to local Storage,
+  // if not remove it
+  if (document.body.classList.contains('light-theme')) {
+    localStorage.setItem('currentTheme', 'light-theme');
+  } else {
+    localStorage.removeItem('currentTheme');
+  }
+});
